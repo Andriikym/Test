2240285d42d138cc718ed450ae99cab5cafaffa0
Andrii Mykhailov <59253912+Andriikym@users.noreply.github.com>
2019-12-30 13:38:01 +0200

    Add docs

--- Sources/Test/Test.swift
+++ Sources/Test/Test.swift
@@ -7,6 +7,8 @@ class Tester {
         print("Test OK")
     }
     
+    /// Prints  your message
+    /// - Parameter message: Text that will be printed
     public func say(_ message: String) {
         print("\(message)")
     }
